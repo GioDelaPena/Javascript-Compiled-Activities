87033e9c5d4ad6f013f089db54667c50791b5141
GioDelaPena <90368122+GioDelaPena@users.noreply.github.com>
2022-05-24 22:01:18 +0800

    Else Statements

---
+++ Else Statements/Exercise1.js
@@ -0,0 +1,8 @@
+var g = 15; 
+if(g == 18){ 
+    console.log("True,", g, "= 18");
+} 
+
+else{
+    console.log("False,", g, "is not equal to 18"); 
+}
---
+++ Else Statements/Exercise2.js
@@ -0,0 +1,8 @@
+var g = 18; 
+if(g == 1 || g >= 20){
+    console.log("True,", g, "= 1 or", g, " > 20");
+}
+
+else{ 
+    console.log("g =", g);
+}
